981219b28ff6d6c2bd7ec804552fdc6c425e1966
apu <40669057+xrayapu@users.noreply.github.com>
2025-08-25 04:43:38 +0600

    Update problem_04.py

--- problem_04.py
+++ problem_04.py
@@ -1,1 +1,23 @@
 
+# reverse a stack:
+
+def insert_it(arr, item):
+   if not arr:
+      return arr.append(item)
+   
+   # got element 
+   it= arr.pop()
+   insert_it(arr, item) # 1st job: last element on 1st position 
+   arr.append(it)
+
+
+def sol(arr):
+   if not arr: # stack empty 
+      return
+
+   item= arr.pop()   
+   sol(arr) # without last element 
+   insert_it(arr,item) # insert the last into it's place 1stly !
+   return arr
+
+print(sol([1,2,3,4,5])) 
